0cd0729bfae19c0b8a2dc62115e44d756bb9e58d
Nyranith <kaalsaas@hotmail.com>
2016-10-02 16:00:13 +0200

    Starting on the asp.net site.

---
+++ Blog-System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Blog_System.MvcApplication" Language="C#" %>
---
+++ Blog-System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog_System.Startup))]
+namespace Blog_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
