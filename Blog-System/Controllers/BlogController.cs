d9616deac11fe2e1ba8b7efe1ce69d73778ccde9
Nyranith <kaalsaas@hotmail.com>
2016-10-09 19:21:28 +0200

    Looking at the asp.net

---
+++ Blog-System/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog_System.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/
+        public ActionResult Index()
+        {
+            return View("blog");
+        }
+	}
+}
---
+++ Blog-System/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog_System.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        public string Index()
+        {
+
+            return "This is something or another"; 
+            //return View();
+        }
+	}
+}
